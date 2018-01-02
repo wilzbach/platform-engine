ad325b4aafb1c6245f54b1048ae64d00247e96a1
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-02 16:43:10 +0100

    feat(setup): add setup.py

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import io
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+
+readme = io.open('README.rst', 'r', encoding='utf-8').read()
+
+setup(
+    name='evenflow',
+    description='Asyncy server',
+    long_description=readme,
+    author='Asyncy',
+    author_email='noreply@asyncy.com',
+    version='0.0.1',
+    packages=find_packages(),
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+        'pytest-mock'
+    ],
+    setup_requires=['pytest-runner'],
+    install_requires=[
+        'celery==4.1.0',
+        'envoy==0.0.3',
+        'pyjwt==1.5.3',
+        'requests==2.18.4',
+        'statsd==3.2.2',
+        'storyscript==0.0.2',
+        'tornpsql==1.1.0'
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ],
+    entry_points="""
+        [console_scripts]
+        evenflow=evenflow.Cli:Cli.main
+    """
+)
