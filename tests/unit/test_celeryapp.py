44f3ffe70c4dea18e27df95fba4197b89cb73253
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-03 11:55:42 +0100

    tests(celeryapp): mock Celery init

--- tests/unit/test_celeryapp.py
+++ tests/unit/test_celeryapp.py
@@ -4,8 +4,9 @@ from celery import Celery
 from evenflow.CeleryApp import CeleryApp
 
 
-def test_celeryapp():
+def test_celeryapp(mocker):
+    mocker.patch.object(Celery, '__init__', return_value=None)
     result = CeleryApp.start()
+    broker = 'amqp://user:password@localhost:5672/vhost'
+    Celery.__init__.assert_called_with('asyncy', broker=broker)
     assert isinstance(result, Celery)
-    assert result.main == 'asyncy'
-    assert result.conf.broker_url == 'amqp://user:password@localhost:5672/vhost'
