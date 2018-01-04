3351e27b55e1bdff7c3d3d0020be32e7789d73ba
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-04 16:34:33 +0100

    feat(containers): Containers.run stores the result

--- evenflow/Containers.py
+++ evenflow/Containers.py
@@ -7,5 +7,6 @@ class Containers:
     def __init__(self, name):
         self.name = name
 
-    def run(self):
-        docker.from_env().containers.run()
+    def run(self, command=None):
+        client = docker.from_env()
+        self.result = client.containers.run(command=command)
--- tests/unit/test_containers.py
+++ tests/unit/test_containers.py
@@ -11,11 +11,22 @@ def container():
     return Containers('hello-world')
 
 
+@fixture
+def docker_mock(mocker):
+    mocker.patch.object(docker, 'from_env')
+
+
 def test_container(container):
     assert container.name == 'hello-world'
 
 
-def test_docker_run(mocker, container):
-    mocker.patch.object(docker, 'from_env')
+def test_docker_run(docker_mock, container):
     container.run()
-    docker.from_env().containers.run.assert_called_with()
+    docker.from_env().containers.run.assert_called_with(command=None)
+    assert container.result == docker.from_env().containers.run()
+
+
+def test_docker_run_command(docker_mock, container):
+    container.run('command')
+    docker.from_env().containers.run.assert_called_with(command='command')
+    assert container.result == docker.from_env().containers.run()
