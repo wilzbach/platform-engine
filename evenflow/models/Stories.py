c4d1a340f69a9c7917a1cf1b1465f99630a7c85a
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-04 13:19:38 +0100

    feat(stories): merge evenflow.Stories with evenflow.models.Stories

--- evenflow/models/Stories.py
+++ evenflow/models/Stories.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from peewee import CharField, ForeignKeyField
 
+import requests
+
 from .Applications import Applications
 from .Base import BaseModel
 from .Repositories import Repositories
@@ -11,3 +13,9 @@ class Stories(BaseModel):
     version = CharField(null=True)
     application = ForeignKeyField(Applications)
     repository = ForeignKeyField(Repositories)
+
+    def get_contents(self):
+        api_url = 'https://api.github.com/repos/{}/{}/contents/{}'
+        file_url = api_url.format(self.repository.owner, self.repository.name,
+                                  self.filename)
+        requests.get(file_url, params={'ref': self.version})
--- tests/unit/models/test_stories.py
+++ tests/unit/models/test_stories.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from evenflow.models.Base import BaseModel
+from evenflow.models.Repositories import Repositories
 from evenflow.models.Stories import Stories
 
 from peewee import CharField, ForeignKeyField
+
+import requests
 
 
 def test_stories():
@@ -12,3 +15,12 @@ def test_stories():
     assert isinstance(Stories.repository, ForeignKeyField)
     assert isinstance(Stories.application, ForeignKeyField)
     assert issubclass(Stories, BaseModel)
+
+
+def test_stories_get(mocker):
+    mocker.patch.object(requests, 'get')
+    repo = Repositories(name='project', owner='user')
+    story = Stories(filename='my.story', repository=repo, application='app')
+    story.get_contents()
+    api_url = 'https://api.github.com/repos/user/project/contents/my.story'
+    requests.get.assert_called_with(api_url, params={'ref': story.version})
