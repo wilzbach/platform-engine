293448f160b00b22bd619101c03bed5b40723a41
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-05 13:22:03 +0100

    feat(github): add Github.get_contents method

--- evenflow/Github.py
+++ evenflow/Github.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import base64
+
+
+import requests
 
 
 class Github:
@@ -17,3 +21,12 @@ class Github:
         if page in pages:
             base = '{}/{}'.format(self.api_url, '{}')
             return base.format(pages[page])
+
+    def make_url(self, page, *args):
+        return self.url(page).format(*args)
+
+    def get_contents(self, organization, repository, file):
+        url = self.make_url('repository', organization, repository, file)
+        response = requests.get(url, params={'ref': None})
+        response.raise_for_status()
+        return base64.b64decode(response.text)
--- tests/unit/test_github.py
+++ tests/unit/test_github.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+import base64
+
 from evenflow.Github import Github
 from evenflow.models import Users
 
 from pytest import fixture, mark
+
+import requests
 
 
 @fixture
@@ -36,3 +40,21 @@ def test_github_url_repository(gh, page, url):
 
 def test_github_url_none(gh):
     assert gh.url('magic') is None
+
+
+def test_github_make_url(mocker, gh):
+    mocker.patch.object(Github, 'url', return_value='test/{}')
+    result = gh.make_url('page', 'argument')
+    Github.url.assert_called_with('page')
+    assert result == 'test/argument'
+
+
+def test_get_contents(mocker, gh):
+    mocker.patch.object(requests, 'get')
+    mocker.patch.object(Github, 'make_url')
+    mocker.patch.object(base64, 'b64decode')
+    result = gh.get_contents('org', 'repo', 'file')
+    Github.make_url.assert_called_with('repository', 'org', 'repo', 'file')
+    requests.get.assert_called_with(Github.make_url(), params={'ref': None})
+    requests.get().raise_for_status.assert_called_with()
+    assert result == base64.b64decode(requests.get().text, altchars=None)
