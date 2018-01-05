1d5e31e06ed93abcd354e1f348758fae947f1fc0
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-05 14:33:52 +0100

    feat(config): add github_app to config defaults

--- evenflow/Config.py
+++ evenflow/Config.py
@@ -7,5 +7,6 @@ class Config(Aratrum):
     default = {
         'database': 'postgresql://postgres:postgres@localhost:5432/database',
         'broker': 'amqp://:@localhost:5672/',
-        'github_pem': 'github.pem'
+        'github_pem': 'github.pem',
+        'github_app': '123456789'
     }
