0c4252a8acfd74071b53e0458d0b1e735a655ab5
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-08 19:49:24 +0100

    feat(tasks): Tasks.process_story get the application

--- evenflow/Tasks.py
+++ evenflow/Tasks.py
@@ -2,7 +2,7 @@
 from playhouse import db_url
 
 from .Config import Config
-from .models import db
+from .models import Applications, db
 
 
 class Tasks:
@@ -10,4 +10,5 @@ class Tasks:
     @staticmethod
     def process_story(app_id, story_name, *, story_id=None):
         db.init(db_url.parse(Config.get('database')))
+        Applications.get(Applications.id == app_id)
         return True
--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -1,29 +1,35 @@
 # -*- coding: utf-8 -*-
 from evenflow.Config import Config
 from evenflow.Tasks import Tasks
-from evenflow.models import db
+from evenflow.models import Applications, db
 
 from playhouse import db_url
 
-from pytest import raises
+from pytest import fixture, raises
 
 
-def test_process_story(mocker):
+@fixture
+def database(mocker):
     mocker.patch.object(db, 'init')
     mocker.patch.object(db_url, 'parse')
+    mocker.patch.object(Applications, 'get')
+
+
+def test_process_story(mocker, database):
     mocker.patch.object(Config, 'get')
     result = Tasks.process_story('app_id', 'story_name')
     Config.get.assert_called_with('database')
     db_url.parse.assert_called_with(Config.get())
     db.init.assert_called_with(db_url.parse())
+    Applications.get.assert_called_with(True)
     assert result
 
 
-def test_process_story_force_keyword():
+def test_process_story_force_keyword(database):
     with raises(TypeError):
         Tasks.process_story('app_id', 'story_name', 'story_id')
 
 
-def test_process_story_with_id():
+def test_process_story_with_id(database):
     result = Tasks.process_story('app_id', 'story_name', story_id='story_id')
     assert result
