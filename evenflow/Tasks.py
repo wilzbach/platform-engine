d962ae38b1a0941291ed85206dda6aed4cf4f095
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-08 19:43:26 +0100

    feat(tasks): Tasks.process_story inits postgres database

--- evenflow/Tasks.py
+++ evenflow/Tasks.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+from playhouse import db_url
+
+from .Config import Config
+from .models import db
 
 
 class Tasks:
 
     @staticmethod
     def process_story(app_id, story_name, *, story_id=None):
+        db.init(db_url.parse(Config.get('database')))
         return True
--- tests/unit/test_tasks.py
+++ tests/unit/test_tasks.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
+from evenflow.Config import Config
 from evenflow.Tasks import Tasks
+from evenflow.models import db
+
+from playhouse import db_url
 
 from pytest import raises
 
 
-def test_process_story():
+def test_process_story(mocker):
+    mocker.patch.object(db, 'init')
+    mocker.patch.object(db_url, 'parse')
+    mocker.patch.object(Config, 'get')
     result = Tasks.process_story('app_id', 'story_name')
+    Config.get.assert_called_with('database')
+    db_url.parse.assert_called_with(Config.get())
+    db.init.assert_called_with(db_url.parse())
     assert result
 
 
