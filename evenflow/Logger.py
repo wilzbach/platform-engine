adf550f3c411c378d1da628d39e7d9aa9e40fbdc
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-18 22:31:39 +0000

    feat(logger): add Logger.start method

--- evenflow/Logger.py
+++ evenflow/Logger.py
@@ -6,3 +6,6 @@ class Logger:
 
     def __init__(self, verbosity=1):
         self.frustum = Frustum(verbosity=1)
+
+    def start(self):
+        self.frustum.register_event('jwt-token', 'debug', 'Encoded token: {}')
--- tests/unit/test_logger.py
+++ tests/unit/test_logger.py
@@ -8,3 +8,12 @@ def test_logger_init(mocker):
     mocker.patch.object(Frustum, '__init__', return_value=None)
     Logger()
     Frustum.__init__.assert_called_with(verbosity=1)
+
+
+def test_logger_start(mocker):
+    mocker.patch.object(Frustum, '__init__', return_value=None)
+    mocker.patch.object(Frustum, 'register_event')
+    logger = Logger()
+    logger.start()
+    message = 'Encoded token: {}'
+    Frustum.register_event.assert_called_with('jwt-token', 'debug', message)
