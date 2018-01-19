9074188ed0624ec5329beaa4c925834beaff688f
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-19 12:33:24 +0000

    feat(repositories): use owner field for repository ownership

--- evenflow/models/Repositories.py
+++ evenflow/models/Repositories.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from peewee import CharField
+from peewee import CharField, ForeignKeyField
 
 from .Base import BaseModel
+from .Users import Users
 
 
 class Repositories(BaseModel):
     name = CharField()
     organization = CharField()
+    owner = ForeignKeyField(Users)
--- tests/unit/models/test_repositories.py
+++ tests/unit/models/test_repositories.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 from evenflow.models import BaseModel, Repositories
 
-from peewee import CharField
+from peewee import CharField, ForeignKeyField
 
 
 def test_repositories():
     assert isinstance(Repositories.name, CharField)
     assert isinstance(Repositories.organization, CharField)
+    assert isinstance(Repositories.owner, ForeignKeyField)
     assert issubclass(Repositories, BaseModel)
