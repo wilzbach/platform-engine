978ea1cebc2d85ce7494cbe2dd8adfe5e1724574
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-19 12:13:53 +0000

    feat(applicationsrepositories): remove applicationsrepositories

--- evenflow/models/__init__.py
+++ evenflow/models/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from .Applications import Applications
-from .ApplicationsRepositories import ApplicationsRepositories
 from .Base import BaseModel, db
 from .Repositories import Repositories
 from .Results import Results
@@ -8,5 +7,5 @@ from .Stories import Stories
 from .Users import Users
 
 
-__all__ = ['Applications', 'ApplicationsRepositories', 'BaseModel',
-           'Repositories', 'Results', 'Stories', 'Users', 'db']
+__all__ = ['Applications', 'BaseModel', 'Repositories', 'Results', 'Stories', 
+           'Users', 'db']
