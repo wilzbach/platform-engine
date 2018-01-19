ee805622dfbc126f679e2083290ee8f132db0bd1
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-19 19:18:06 +0000

    feat(http): remove base64 transformation and add json switch

--- evenflow/Http.py
+++ evenflow/Http.py
@@ -1,30 +1,22 @@
 # -*- coding: utf-8 -*-
-import base64
-
 import requests
 
 
 class Http:
 
-    @staticmethod
-    def _transform(response, transformation):
-        if transformation == 'base64':
-            return base64.b64decode(response.text)
-        elif transformation == 'json':
+    @classmethod
+    def _call(cls, method, url, json=False, **kwargs):
+        request = getattr(requests, method)
+        response = request(url, **kwargs)
+        response.raise_for_status()
+        if json:
             return response.json()
         return response.text
 
     @classmethod
-    def _call(cls, method, url, transformation=None, **kwargs):
-        request = getattr(requests, method)
-        response = request(url, **kwargs)
-        response.raise_for_status()
-        return cls._transform(response, transformation)
+    def get(cls, url, json=False, **kwargs):
+        return cls._call('get', url, json=json, **kwargs)
 
     @classmethod
-    def get(cls, url, transformation=None, **kwargs):
-        return cls._call('get', url, transformation=transformation, **kwargs)
-
-    @classmethod
-    def post(cls, url, transformation=None, **kwargs):
-        return cls._call('post', url, transformation=transformation, **kwargs)
+    def post(cls, url, json=False, **kwargs):
+        return cls._call('post', url, json=json, **kwargs)
--- tests/unit/test_http.py
+++ tests/unit/test_http.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-import base64
-
 from evenflow.Http import Http
 
 from pytest import fixture, mark
@@ -12,12 +10,6 @@ import requests
 def requests_mocks(mocker):
     mocker.patch.object(requests, 'get')
     mocker.patch.object(requests, 'post')
-
-
-@fixture
-def b64decode(mocker):
-    mocker.patch.object(base64, 'b64decode')
-    return base64.b64decode
 
 
 @mark.parametrize('method', ['get', 'post'])
@@ -39,14 +31,6 @@ def test_http_methods_kwargs(requests_mocks, method):
 
 
 @mark.parametrize('method', ['get', 'post'])
-def test_http_base64_transformation(b64decode, requests_mocks, method):
-    response = getattr(Http, method)('url', transformation='base64')
-    request_method = getattr(requests, method)
-    b64decode.assert_called_with(request_method().text)
-    assert response == b64decode()
-
-
-@mark.parametrize('method', ['get', 'post'])
 def test_http_get_json(requests_mocks, method):
-    response = getattr(Http, method)('url', transformation='json')
+    response = getattr(Http, method)('url', json=True)
     assert response == getattr(requests, method)().json()
