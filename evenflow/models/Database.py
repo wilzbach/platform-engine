af0676b0751960d5803bfa837694e60bf11bc9cf
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-22 15:47:55 +0000

    feat(database): subclass PostgresqlDatabase

--- evenflow/models/Database.py
+++ evenflow/models/Database.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from peewee import PostgresqlDatabase
 
+from playhouse import db_url
 
-database = PostgresqlDatabase('database',
-                              user='postgres',
-                              password='postgres',
-                              host='localhost'
-                              )
+
+class Database(PostgresqlDatabase):
+
+    def from_url(self, database_url):
+        db_dict = db_url.parse(database_url)
+        super().init(db_dict['database'], host=db_dict['host'],
+                     port=db_dict['port'], user=db_dict['user'],
+                     password=db_dict['password'])
--- tests/unit/models/test_database.py
+++ tests/unit/models/test_database.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
-from evenflow.models import database
+from evenflow.models.Database import Database
 
 from peewee import PostgresqlDatabase
 
+from playhouse import db_url
+
+from pytest import fixture
+
+
+@fixture
+def database():
+    return Database(None)
+
 
 def test_database():
-    assert isinstance(database, PostgresqlDatabase)
+    assert issubclass(Database, PostgresqlDatabase)
+
+
+def test_database_from_url(mocker, database):
+    db_dict = {'database': 'db', 'host': 'host', 'port': 'port',
+               'user': 'user', 'password': 'password'}
+    mocker.patch.object(PostgresqlDatabase, 'init')
+    mocker.patch.object(db_url, 'parse', return_value=db_dict)
+    database.from_url('dburl')
+    db_url.parse.assert_called_with('dburl')
+    PostgresqlDatabase.init.assert_called_with('db', host='host', port='port',
+                                               user='user',
+                                               password='password')
