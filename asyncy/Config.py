bd3984ffcc821d62a82ef0230547f52c5e211800
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-29 15:36:07 +0000

    feat(config): add logger.verbosity to defaults

--- asyncy/Config.py
+++ asyncy/Config.py
@@ -8,6 +8,7 @@ class Config():
         'database': 'postgresql://postgres:postgres@localhost:5432/asyncy',
         'mongo': 'mongodb://localhost:27017/',
         'broker': 'amqp://:@localhost:5672/',
+        'logger.verbosity': 1,
         'github.app_name': 'myapp',
         'github.pem_path': 'github.pem',
         'github.app_identifier': '123456789'
