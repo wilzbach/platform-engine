e5df6dc6cb952b323e841fe2b5f8bd4687f3f538
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-01-29 20:02:21 +0000

    fix(applications): use HStoreField instead of JSONField

--- asyncy/models/Applications.py
+++ asyncy/models/Applications.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from peewee import CharField, ForeignKeyField
 
-from playhouse.postgres_ext import JSONField
+from playhouse.postgres_ext import HStoreField
 
 from .Base import BaseModel
 from .Stories import Stories
@@ -12,7 +12,7 @@ class Applications(BaseModel):
 
     name = CharField()
     user = ForeignKeyField(Users)
-    initial_data = JSONField(null=True)
+    initial_data = HStoreField(null=True)
 
     def get_story(self, story_name):
         appstory = self.stories.join(Stories)\
--- tests/unit/models/test_applications.py
+++ tests/unit/models/test_applications.py
@@ -3,13 +3,13 @@ from asyncy.models import Applications, BaseModel, Stories
 
 from peewee import CharField, ForeignKeyField
 
-from playhouse.postgres_ext import JSONField
+from playhouse.postgres_ext import HStoreField
 
 
 def test_applications():
     assert isinstance(Applications.name, CharField)
     assert isinstance(Applications.user, ForeignKeyField)
-    assert isinstance(Applications.initial_data, JSONField)
+    assert isinstance(Applications.initial_data, HStoreField)
     assert Applications.initial_data.null is True
     assert issubclass(Applications, BaseModel)
 
