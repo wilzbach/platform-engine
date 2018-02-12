9014d9eba828a6e4a5a7086a15622a8670f3dce3
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-02-12 17:35:20 +0000

    chore(utils): remove unused Document module

--- asyncy/utils/__init__.py
+++ asyncy/utils/__init__.py
@@ -1,6 +1,5 @@
-from .Document import Document
 from .Http import Http
 from .Jwt import Jwt
 from .Yaml import Yaml
 
-__all__ = ['Document', 'Http', 'Jwt', 'Yaml']
+__all__ = ['Http', 'Jwt', 'Yaml']
