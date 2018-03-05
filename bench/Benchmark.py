e805e83c44cf6a68874d39d3c23c38f768dcbc06
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-03-05 13:02:39 +0000

    feat(bench): add benchmark placeholder

---
+++ bench/Benchmark.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+class Benchmark:
+
+    """
+    Placeholder for future benchmarking functionalities.
+
+    It should benchmark running N containers versus running N containers from
+    stories.
+    """
+    pass
