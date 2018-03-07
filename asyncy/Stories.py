3e11d72b25ab3d91d9a1016ca2b9970ac9a38e08
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-03-07 11:49:57 +0000

    feat(stories): add Stories module that queries api for story data

---
+++ asyncy/Stories.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from .utils import Http
+
+
+class Stories:
+
+    def __init__(self, logger, app_id, story_name):
+        self.app_id = app_id
+        self.name = story_name
+        self.logger = logger
+
+    def get(self):
+        url = 'http://api/apps/{}/stories/{}'.format(self.app_id, self.name)
+        story = Http.get(url, json=True)
+        self.tree = story['tree']
+        self.environment = story['environment']
+        self.containers = story['containers']
+        self.repository = story['repository']
---
+++ tests/unit/Stories.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from asyncy.Stories import Stories
+from asyncy.utils import Http
+
+from pytest import fixture
+
+
+@fixture
+def story(logger):
+    return Stories(logger, 1, 'hello.story')
+
+
+def test_stories_init(logger, story):
+    assert story.app_id == 1
+    assert story.name == 'hello.story'
+    assert story.logger == logger
+
+
+def test_stories_get(patch, story):
+    patch.object(Http, 'get')
+    story.get()
+    url = 'http://api/apps/1/stories/hello.story'
+    Http.get.assert_called_with(url, json=True)
+    assert story.tree == Http.get()['tree']
+    assert story.environment == Http.get()['environment']
+    assert story.containers == Http.get()['containers']
+    assert story.repository == Http.get()['repository']
