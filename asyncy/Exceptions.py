34e1a852f95088a2bd33b6755a3c727fd9388e68
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-03-07 15:42:10 +0000

    fix(exceptions): remove GithubAuthError exception

--- asyncy/Exceptions.py
+++ asyncy/Exceptions.py
@@ -5,9 +5,3 @@ class AsyncyError(Exception):
 
     def __init__(self, message=None):
         super().__init__(message)
-
-
-class GithubAuthError(AsyncyError):
-    def __init__(self, github_app, installation_id):
-        message = 'Cannot authenticate app {} with installation id {}'
-        super().__init__(message.format(github_app, installation_id))
--- tests/unit/Exceptions.py
+++ tests/unit/Exceptions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from asyncy.Exceptions import AsyncyError, GithubAuthError
+from asyncy.Exceptions import AsyncyError
 
 from pytest import raises
 
@@ -7,8 +7,3 @@ from pytest import raises
 def test_asyncy_error():
     with raises(AsyncyError):
         raise AsyncyError('things happen')
-
-
-def test_github_auth_error():
-    with raises(GithubAuthError):
-        raise GithubAuthError('app', 'installation')
