0242f91316640aa2ddadef2713cb782957bb7b59
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-03-08 18:27:38 +0000

    tests(conftest): config fixture returns a MagicMock

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from asyncy.Config import Config
 from asyncy.Stories import Stories
 
 from pytest import fixture
@@ -47,8 +46,8 @@ def logger(magic):
 
 
 @fixture
-def config(mocker):
-    return Config()
+def config(magic):
+    return magic()
 
 
 @fixture
