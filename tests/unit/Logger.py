729c31bd7683db3e062f1792bd0f1189b075a38f
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-03-21 16:19:25 +0000

    fix(logger): Logger.start does not call Logger.adapter

--- tests/unit/Logger.py
+++ tests/unit/Logger.py
@@ -109,13 +109,12 @@ def test_logger_adapter(patch, magic, logger):
 
 def test_logger_start(patch, logger):
     patch.many(Frustum, ['register_event', 'start_logger'])
-    patch.many(logger, ['add_logdna', 'adapter'])
+    patch.object(logger, 'add_logdna')
     logger.events = [('event', 'level', 'message')]
     logger.start()
     Frustum.register_event.assert_called_with('event', 'level', 'message')
     Frustum.start_logger.assert_called_with()
     assert logger.add_logdna.call_count == 1
-    assert logger.frustum.logger == logger.adapter()
 
 
 def test_logger_log(patch, logger):
