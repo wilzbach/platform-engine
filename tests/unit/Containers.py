21db5f3956cf5f57c20732601c38b936e39d30c2
Jude Pereira <judebpereira@gmail.com>
2018-05-27 14:06:19 +0530

    Handle docker TLS.

--- tests/unit/Containers.py
+++ tests/unit/Containers.py
@@ -39,6 +39,8 @@ async def test_container_exec(patch, story, app, logger, async_mock):
     fetch = AsyncHTTPClient.fetch.mock
 
     endpoint = app.config.DOCKER_HOST
+    if story.app.config.DOCKER_TLS_VERIFY == '1':
+        endpoint = endpoint.replace('http://', 'https://')
 
     assert fetch.mock_calls[0][1][1] == \
         '{0}/v1.37/containers/alpine/exec'.format(endpoint)
