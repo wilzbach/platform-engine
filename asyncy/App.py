89ca5e41a88b79e3a4dc11a43dc13e4384de92ce
Jude Pereira <judebpereira@gmail.com>
2018-06-03 19:33:33 +0530

    Skel for bootstrap - execute all stories on boot.

--- asyncy/App.py
+++ asyncy/App.py
@@ -2,6 +2,8 @@
 import os
 from json import load
 
+from .processing import Story
+
 
 class App:
 
@@ -27,3 +29,12 @@ class App:
         self.environment = self.load_file('environment.json')
         self.stories = self.load_file('stories.json')
         self.services = self.load_file('services.json')
+
+    def bootstrap(self):
+        """
+        Executes all the stories.
+        This enables the story to listen to pub/sub,
+        register with the gateway, and queue cron jobs.
+        """
+        for story_name in self.stories:
+            Story.story(self, logger, story_name)
