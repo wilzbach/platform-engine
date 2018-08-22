3e9cc83793a981acc75b5c5f0894d7f73643effe
Jude Pereira <contact@judepereira.com>
2018-08-22 21:03:57 +0530

    Condense http/post and http/get into http/fetch.

--- asyncy/processing/internal/Http.py
+++ asyncy/processing/internal/Http.py
@@ -5,14 +5,18 @@ from .Decorators import Decorators
 from ...utils.HttpUtils import HttpUtils
 
 
-@Decorators.create_service(name='http', command='post', arguments={
+@Decorators.create_service(name='http', command='fetch', arguments={
     'url': {'type': 'string'},
     'headers': {'type': 'map'},
-    'body': {'type': 'string'}
+    'body': {'type': 'string'},
+    'method': {'type': 'string'}
 }, output_type='any')
 async def http_post(story, line, resolved_args):
+    method = resolved_args.get('method', 'get') or 'get'
     http_client = AsyncHTTPClient()
-    kwargs = _make_kwargs('POST', resolved_args.get('headers'))
+    kwargs = {'method': method.upper()}
+    if resolved_args.get('headers'):
+        kwargs['headers'] = resolved_args.get('headers')
 
     if resolved_args.get('body'):
         kwargs['body'] = resolved_args['body']
@@ -20,27 +24,6 @@ async def http_post(story, line, resolved_args):
     return await HttpUtils.fetch_with_retry(1, story.logger,
                                             resolved_args['url'],
                                             http_client, kwargs)
-
-
-@Decorators.create_service(name='http', command='get', arguments={
-    'url': {'type': 'string'},
-    'headers': {'type': 'map'}
-}, output_type='any')
-async def http_get(story, line, resolved_args):
-    http_client = AsyncHTTPClient()
-    kwargs = _make_kwargs('GET', resolved_args.get('headers'))
-
-    return await HttpUtils.fetch_with_retry(1, story.logger,
-                                            resolved_args['url'],
-                                            http_client, kwargs)
-
-
-def _make_kwargs(method, headers):
-    kwargs = {'method': method}
-    if headers:
-        kwargs['headers'] = headers
-
-    return kwargs
 
 
 def init():
