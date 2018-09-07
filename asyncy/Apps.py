957fb94df41753d9f67b852532eda44daa5c162a
Jude Pereira <contact@judepereira.com>
2018-09-07 23:30:56 +0530

    Reload apps on Postgres 'release' notification event.

--- asyncy/Apps.py
+++ asyncy/Apps.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import asyncio
+import select
+import threading
+
 import psycopg2
 
 from raven.contrib.tornado import AsyncSentryClient
@@ -16,6 +20,11 @@ class Apps:
     sentry_client = None
 
     @classmethod
+    def new_pg_conn(cls):
+        return psycopg2.connect(database='asyncy', user='postgres',
+                                options=f'-c search_path=app_public')
+
+    @classmethod
     def _init_sentry(cls, sentry_dsn: str, release: str):
         cls.sentry_client = AsyncSentryClient(
             dsn=sentry_dsn,
@@ -24,8 +33,7 @@ class Apps:
 
     @classmethod
     def get_releases(cls):
-        conn = psycopg2.connect(database='asyncy', user='postgres',
-                                options=f'-c search_path=app_public')
+        conn = cls.new_pg_conn()
         cur = conn.cursor()
 
         query = """
@@ -41,6 +49,35 @@ class Apps:
         return cur.fetchall()
 
     @classmethod
+    async def deploy_release(cls, config, logger, app_id, version, environment,
+                             stories,
+                             maintenance):
+        logger.info(f'Deploying app {app_id}@{version}')
+        if maintenance:
+            logger.info(f'Deployment halted {app_id}@{version}')
+            return
+
+        try:
+            Sentry.clear_and_set_context(cls.sentry_client,
+                                         app_id, version)
+
+            services = await cls._prepare_services(
+                stories.get('yaml', {}), logger, stories)
+
+            app = App(app_id, version, config, logger,
+                      stories, services, environment,
+                      sentry_client=cls.sentry_client)
+
+            await app.bootstrap()
+
+            cls.apps[app_id] = app
+            logger.info(f'Successfully deployed app {app_id}@{version}')
+        except BaseException as e:
+            logger.error(
+                f'Failed to bootstrap app {app_id}@{version}', exc=e)
+            cls.sentry_client.capture('raven.events.Exception')
+
+    @classmethod
     async def init_all(cls, sentry_dsn: str, release: str,
                        config: Config, logger: Logger):
         cls._init_sentry(sentry_dsn, release)
@@ -53,29 +90,14 @@ class Apps:
             environment = release[2]
             stories = release[3]
             maintenance = release[4]
-            logger.info(f'Deploying app {app_id}@{version}')
-            if maintenance:
-                continue
+            await cls.deploy_release(config, logger, app_id, version,
+                                     environment, stories, maintenance)
 
-            try:
-                Sentry.clear_and_set_context(cls.sentry_client,
-                                             app_id, version)
-
-                services = await cls._prepare_services(
-                    stories.get('yaml', {}), logger, stories)
-
-                app = App(app_id, version, config, logger,
-                          stories, services, environment,
-                          sentry_client=cls.sentry_client)
-
-                await app.bootstrap()
-
-                cls.apps[app_id] = app
-                logger.info(f'Successfully deployed app {app_id}@{version}')
-            except BaseException as e:
-                logger.error(
-                    f'Failed to bootstrap app {app_id}@{version}', exc=e)
-                cls.sentry_client.capture('raven.events.Exception')
+        loop = asyncio.get_event_loop()
+        t = threading.Thread(target=cls.listen_to_releases,
+                             args=[config, logger, loop],
+                             daemon=True)
+        t.start()
 
     @classmethod
     def get(cls, app_id: str):
@@ -111,13 +133,75 @@ class Apps:
         return services
 
     @classmethod
-    async def destroy_app(cls, app: App):
+    async def destroy_app(cls, app: App, silent=False):
         app.logger.info(f'Destroying app {app.app_id}')
-        await app.destroy()
+        try:
+            await app.destroy()
+        except BaseException as e:
+            if not silent:
+                raise e
+            app.logger.error(
+                f'Failed to destroy app {app.app_id}@{app.version}; '
+                f'will eat exception (silent=True)',
+                exc=e)
+
         app.logger.info(f'Completed destroying app {app.app_id}')
         cls.apps[app.app_id] = None
+
+    @classmethod
+    async def reload_app(cls, config: Config, logger: Logger, app_id: str):
+        logger.info(f'Reloading app {app_id}')
+        if cls.apps.get(app_id) is not None:
+            await cls.destroy_app(cls.apps[app_id], silent=True)
+        try:
+            conn = cls.new_pg_conn()
+
+            curs = conn.cursor()
+            query = """
+            with latest as (select app_uuid, max(id) as id
+                from releases group by app_uuid)
+            select app_uuid, id, config, payload, maintenance
+            from latest
+                   inner join releases using (app_uuid, id)
+                   inner join apps on (latest.app_uuid = apps.uuid)
+            where app_uuid = %s;
+            """
+            curs.execute(query, (app_id,))
+            release = curs.fetchone()
+            app_id = release[0]
+            version = release[1]
+            environment = release[2]
+            stories = release[3]
+            maintenance = release[4]
+            await cls.deploy_release(config, logger, app_id, version,
+                                     environment, stories, maintenance)
+            logger.info(f'Reloaded app {app_id}@{version}')
+        except BaseException as e:
+            logger.error(
+                f'Failed to reload app {app_id}', exc=e)
+            cls.sentry_client.capture('raven.events.Exception')
 
     @classmethod
     async def destroy_all(cls):
         for app in cls.apps.values():
             await cls.destroy_app(app)
+
+    @classmethod
+    def listen_to_releases(cls, config: Config, logger: Logger, loop):
+        logger.info('Listening for new releases...')
+        conn = cls.new_pg_conn()
+        conn.set_isolation_level(
+            psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+
+        curs = conn.cursor()
+        curs.execute('listen release;')
+
+        while True:
+            if select.select([conn], [], [], 5) == ([], [], []):
+                pass
+            else:
+                conn.poll()
+                while conn.notifies:
+                    notify = conn.notifies.pop(0)
+                    asyncio.run_coroutine_threadsafe(
+                        cls.reload_app(config, logger, notify.payload), loop)
