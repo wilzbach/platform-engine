9531a7407b8ff1a7a8789da9b8201a267f5bcba1
Jude Pereira <contact@judepereira.com>
2018-09-10 11:29:11 +0530

    Refactor logger_level, logger_name to LOGGER_LEVEL, LOGGER_NAME.

--- tests/unit/Config.py
+++ tests/unit/Config.py
@@ -17,9 +17,8 @@ def config():
 
 
 def test_config():
-    # TODO: These constants are now upper cased
-    assert Config.defaults['logger_name'] == 'asyncy'
-    assert Config.defaults['logger_level'] == 'debug'
+    assert Config.defaults['LOGGER_NAME'] == 'asyncy'
+    assert Config.defaults['LOGGER_LEVEL'] == 'debug'
 
 
 def test_config_init(patch):
