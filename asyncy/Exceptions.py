e43018a518beeed31eec8d76646e76260b8b3cd1
Jude Pereira <contact@judepereira.com>
2018-10-04 20:15:57 +0530

    Remove DockerError.

--- asyncy/Exceptions.py
+++ asyncy/Exceptions.py
@@ -34,12 +34,6 @@ class InvalidCommandError(AsyncyError):
         super().__init__(message, story=story, line=line)
 
 
-class DockerError(AsyncyError):
-
-    def __init__(self, story=None, line=None, message=None):
-        super().__init__(message, story=story, line=line)
-
-
 class K8sError(AsyncyError):
 
     def __init__(self, story=None, line=None, message=None):
