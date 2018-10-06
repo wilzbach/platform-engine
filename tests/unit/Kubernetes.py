b9f3da99caa696c8b3d860288f47de6235240032
Jude Pereira <contact@judepereira.com>
2018-10-06 14:45:06 +0530

    Add tests for is_2xx and raise_if_not_2xx.

--- tests/unit/Kubernetes.py
+++ tests/unit/Kubernetes.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
+from unittest.mock import MagicMock
+
+from asyncy.Exceptions import K8sError
 from asyncy.Kubernetes import Kubernetes
+
+import pytest
+from pytest import fixture
+
+
+@fixture
+def line():
+    return MagicMock()
 
 
 def test_find_all_ports():
@@ -44,3 +55,25 @@ def test_find_all_ports():
     assert Kubernetes.find_all_ports(services['alpine']) == {8080}
     assert Kubernetes.find_all_ports(services['alpha']) == {9090, 9091, 9092}
     assert Kubernetes.find_all_ports(services['nested']) == {1234, 1235}
+
+
+def test_raise_if_not_2xx(story, line):
+    res = MagicMock()
+    res.code = 401
+    with pytest.raises(K8sError):
+        Kubernetes.raise_if_not_2xx(res, story, line)
+
+    res.code = 200
+    assert Kubernetes.raise_if_not_2xx(res, story, line) is None
+
+
+def test_is_2xx():
+    res = MagicMock()
+    res.code = 200
+    assert Kubernetes.is_2xx(res) is True
+    res.code = 210
+    assert Kubernetes.is_2xx(res) is True
+    res.code = 300
+    assert Kubernetes.is_2xx(res) is False
+    res.code = 400
+    assert Kubernetes.is_2xx(res) is False
