e0a273f9a9ff477036a58ac623e708507e29f562
Jude Pereira <contact@judepereira.com>
2018-10-12 17:33:34 +0530

    Remove extra methods.

--- asyncy/processing/Resolver.py
+++ asyncy/processing/Resolver.py
@@ -12,16 +12,6 @@ class Resolver:
         return item[index]
 
     @classmethod
-    def handside(cls, item, data):
-        return cls.object(item, data)
-
-    @staticmethod
-    def stringify(value):
-        if type(value) is str:
-            return '"""{}"""'.format(value.replace('"', '\"'))
-        return str(value)
-
-    @classmethod
     def values(cls, items_list, data):
         """
         Parses a list of values objects. The list may contain other objects.
@@ -43,13 +33,6 @@ class Resolver:
         return string
 
     @classmethod
-    def file(cls, filename, data, values=None):
-        if values:
-            values = Resolver.values(values, data)
-            return filename.format(*values)
-        return filename
-
-    @classmethod
     def path(cls, paths, data):
         """
         Resolves a path against some data, for example the path ['a', 'b']
@@ -59,10 +42,6 @@ class Resolver:
             return reduce(cls._walk, paths, data)
         except (KeyError, TypeError):
             return None
-
-    @classmethod
-    def argument(cls, argument, data):
-        return cls.object(argument, data)
 
     @classmethod
     def dictionary(cls, dictionary, data):
@@ -76,29 +55,6 @@ class Resolver:
         for item in items:
             yield cls.resolve(item, data)
 
-    @staticmethod
-    def method(method, left, right):
-        if method == 'like':
-            return right.match(left) is not None
-        elif method == 'notlike':
-            return right.match(left) is None
-        elif method == 'in':
-            return left in right
-        elif method in ('has', 'contains'):
-            return right in left
-        elif method == 'excludes':
-            return left not in right
-        elif method == 'isnt':
-            return right != left
-        elif method == 'is':
-            return right == left
-        raise ValueError('Method not supported')
-
-    @classmethod
-    def expression(cls, data, expression, values):
-        mapping = map(cls.stringify, cls.values(values, data=data))
-        return eval(expression.format(*mapping))
-
     @classmethod
     def object(cls, item, data):
         object_type = item.get('$OBJECT')
@@ -106,26 +62,12 @@ class Resolver:
             if 'values' in item:
                 return cls.string(item['string'], data, values=item['values'])
             return cls.string(item['string'], data)
-        elif object_type == 'file':
-            if 'values' in item:
-                return cls.file(item['string'], data, values=item['values'])
-            return cls.file(item['string'], data)
         elif object_type == 'path':
             return cls.path(item['paths'], data)
         elif object_type == 'regexp':
             return re.compile(item['regexp'])
         elif object_type == 'value':
             return item['value']
-        elif object_type == 'method':
-            left = cls.handside(item['left'], data)
-            right = cls.handside(item['right'], data)
-            return cls.method(item['method'], left, right)
-        elif object_type == 'expression':
-            expression = item['expression']
-            values = item['values']
-            return cls.expression(data, expression, values)
-        elif object_type == 'argument':
-            return cls.argument(item['argument'], data)
         elif object_type == 'dict':
             return dict(cls.dict(item['items'], data))
         elif object_type == 'list':
