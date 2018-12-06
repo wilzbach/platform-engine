25e3b5a7b56d017065efcf8680c3d788c30b485b
Jude Pereira <contact@judepereira.com>
2018-12-06 15:43:38 +0530

    Retry when the error code is 599 - network errors.

--- asyncy/utils/HttpUtils.py
+++ asyncy/utils/HttpUtils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import asyncio
 import urllib
 from urllib import parse
 
@@ -11,17 +12,26 @@ class HttpUtils:
     async def fetch_with_retry(tries, logger, url, http_client, kwargs):
         kwargs['raise_error'] = False
         attempts = 0
+        last_exception = None
         while attempts < tries:
             attempts = attempts + 1
             try:
-                return await http_client.fetch(url, **kwargs)
+                res = await http_client.fetch(url, **kwargs)
+                if res.code == 599:  # Network connectivity issues.
+                    raise HTTPError(res.code, message=str(res.error),
+                                    response=res)
+                return res
             except HTTPError as e:
+                last_exception = e
                 logger.log_raw(
                     'error',
                     f'Failed to call {url}; attempt={attempts}; err={str(e)}'
                 )
+                await asyncio.sleep(0.5)
 
-        raise HTTPError(500, message=f'Failed to call {url}!')
+        assert last_exception is not None  # Impossible.
+        raise HTTPError(500, message=f'Failed to call {url}!') \
+            from last_exception
 
     @staticmethod
     def add_params_to_url(url, params):
