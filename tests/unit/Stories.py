514699373fb820cb011b4e12522cd8c4bf02416d
judepereira <contact@judepereira.com>
2019-01-17 10:31:05 +0100

    Don't auto convert bytes to string.

--- tests/unit/Stories.py
+++ tests/unit/Stories.py
@@ -150,7 +150,7 @@ def test_stories_end_line_output_as_bytes(patch, story):
     patch.object(time, 'time')
     story.results = {'1': {'start': 'start'}}
     story.end_line('1', output=b'output')
-    assert story.results['1']['output'] == 'output'
+    assert story.results['1']['output'] == b'output'
 
 
 @mark.parametrize('input,output', [
