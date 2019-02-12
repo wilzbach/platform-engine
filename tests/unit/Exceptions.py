df8029d9e9b7e7bf04f8a4100ddbf9197a43a60d
judepereira <contact@judepereira.com>
2019-02-12 19:19:41 +0530

    Add tests to cover new exceptions.

--- tests/unit/Exceptions.py
+++ tests/unit/Exceptions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from asyncy.Exceptions import AsyncyError
+from asyncy.Exceptions import AsyncyError, TooManyServices, TooManyVolumes
 
 from pytest import raises
 
@@ -7,3 +7,13 @@ from pytest import raises
 def test_asyncy_error():
     with raises(AsyncyError):
         raise AsyncyError('things happen')
+
+
+def test_many_volumes():
+    with raises(TooManyVolumes):
+        raise TooManyVolumes(10, 10)
+
+
+def test_many_services():
+    with raises(TooManyServices):
+        raise TooManyServices(10, 10)
