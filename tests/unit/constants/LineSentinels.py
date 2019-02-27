ff50bb1f959dc8c412f93cbcb868d836689291f4
judepereira <contact@judepereira.com>
2019-02-27 16:01:18 +0530

    Add tests.

---
+++ tests/unit/constants/LineSentinels.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from asyncy.constants.LineSentinels import LineSentinels, _Sentinel
+
+
+def test_sentinel_class():
+    s = _Sentinel('hello')
+    assert str(s) == '_Sentinel#hello'
+    assert LineSentinels.is_sentinel(s)
+    assert not LineSentinels.is_not_sentinel(s)
+
+
+def test_sentinels():
+    assert LineSentinels.BREAK is not None
+    assert LineSentinels.RETURN is not None
