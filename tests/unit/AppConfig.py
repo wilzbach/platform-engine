1bef00b5dca3d4bfc5916eeec66cd70d45fc6d43
judepereira <contact@judepereira.com>
2019-04-08 17:17:16 +0200

    Add test for AppConfig.

---
+++ tests/unit/AppConfig.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from operator import attrgetter
+
+from asyncy.AppConfig import AppConfig
+
+
+def test_app_config():
+    raw = {
+        'expose_0': {
+            'service': 'service_0',
+            'name': 'expose_name_0',
+            'http': {
+                'path': '/my_expose_path_0'
+            }
+        },
+        'expose_1': {
+            'service': 'service_1',
+            'name': 'expose_name_1',
+            'http': {
+                'path': '/my_expose_path_1'
+            }
+        }
+    }
+    config = AppConfig({'expose': raw})
+    exposes = config.get_expose_config()
+
+    assert len(exposes) == 2
+
+    for i in range(0, 2):
+        assert exposes[i].service == f'service_{i}'
+        assert exposes[i].http_path == f'/my_expose_path_{i}'
+        assert exposes[i].service_expose_name == f'expose_name_{i}'
+        assert exposes[i].name == f'expose_{i}'
