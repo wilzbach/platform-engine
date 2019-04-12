b590a115e1161efe2ca7dd961ccde5b1bbec9631
judepereira <contact@judepereira.com>
2019-04-12 14:48:53 +0200

    Add comment for all the missing tests.

--- tests/unit/utils/Resolver.py
+++ tests/unit/utils/Resolver.py
@@ -3,6 +3,8 @@ from asyncy.utils import Resolver
 
 import pytest
 
+# Note: Actual resolution tests for resolution are in integration/Lexicon.
+
 
 def test_expression_invalid_type():
     with pytest.raises(Exception):
