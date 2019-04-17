9b13fd3e665b936645f94d707c70c97cc4b9fb0d
Sebastian Wilzbach <seb@wilzba.ch>
2019-04-17 11:39:46 +0200

    Rename contains key for consistency with string and list

--- asyncy/processing/mutations/DictMutations.py
+++ asyncy/processing/mutations/DictMutations.py
@@ -34,5 +34,5 @@ class DictMutations:
 
     @classmethod
     def contains(cls, mutation, value, story, line, operator):
-        key = story.argument_by_name(mutation, 'key')
+        key = story.argument_by_name(mutation, 'item')
         return value.get(key) is not None
