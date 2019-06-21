dc94ba2abdb26067290658f34b99999c8d02e735
Jude Pereira <contact@judepereira.com>
2019-06-21 15:57:04 +0200

    Clean up tests for database using fixtures.
    Signed-off-by: Jude Pereira <contact@judepereira.com>

--- tests/unit/Database.py
+++ tests/unit/Database.py
@@ -1,12 +1,21 @@
+# -*- coding: utf-8 -*-
 from asyncy.db.Database import Database
 from asyncy.entities.ContainerConfig import ContainerConfig
 from asyncy.entities.Release import Release
 from asyncy.enums.ReleaseState import ReleaseState
 
+from pytest import fixture
 
-def test_update_release_state(patch, magic, logger, config):
-    conn = magic()
-    patch.object(Database, 'new_pg_conn', return_value=conn)
+
+@fixture
+def database(magic, patch):
+    db = magic()
+    patch.object(Database, 'new_pg_cur')
+    Database.new_pg_cur.return_value.__enter__.return_value = db
+    return db
+
+
+def test_update_release_state(logger, config, database):
     expected_query = 'update releases ' \
                      'set state = %s ' \
                      'where app_uuid = %s and id = %s;'
@@ -14,30 +23,20 @@ def test_update_release_state(patch, magic, logger, config):
     Database.update_release_state(logger, config, 'app_id', 'version',
                                   ReleaseState.DEPLOYED)
 
-    Database.new_pg_conn.assert_called_with(config)
-    conn.cursor().execute.assert_called_with(
+    database.cur.execute.assert_called_with(
         expected_query, (ReleaseState.DEPLOYED.value, 'app_id', 'version'))
 
-    conn.commit.assert_called()
-    conn.cursor().close.assert_called()
-    conn.close.assert_called()
+    database.conn.commit.assert_called()
 
 
-def test_get_all_app_uuids_for_deployment(patch, magic, config):
-    conn = magic()
-    patch.object(Database, 'new_pg_conn', return_value=conn)
+def test_get_all_app_uuids_for_deployment(config, database):
     query = 'select app_uuid uuid from releases group by app_uuid;'
     ret = Database.get_all_app_uuids_for_deployment(config)
-    conn.cursor().execute.assert_called_with(query)
-    assert ret == conn.cursor().fetchall()
-
-    conn.cursor().close.assert_called()
-    conn.close.assert_called()
+    database.cur.execute.assert_called_with(query)
+    assert ret == database.cur.fetchall()
 
 
-def test_get_container_configs(patch, magic, config):
-    conn = magic()
-    patch.object(Database, 'new_pg_conn', return_value=conn)
+def test_get_container_configs(patch, magic, config, database):
     expected_query = """
             with containerconfigs as (select name, owner_uuid, containerconfig,
                                              json_object_keys(
@@ -49,7 +48,7 @@ def test_get_container_configs(patch, magic, config):
             where owner_uuid = %s and registry = %s
             """
 
-    patch.object(conn.cursor(), 'fetchall', return_value=[
+    patch.object(database.cur, 'fetchall', return_value=[
         {'name': 'n1', 'containerconfig': 'config'}
     ])
     app = magic()
@@ -62,17 +61,12 @@ def test_get_container_configs(patch, magic, config):
         ContainerConfig(name='n1', data='config')
     ]
 
-    conn.cursor().execute.assert_called_with(expected_query,
-                                             (app.owner_uuid, registry_url))
-
-    conn.cursor().close.assert_called()
-    conn.close.assert_called()
+    database.cur.execute.assert_called_with(expected_query,
+                                            (app.owner_uuid, registry_url))
 
 
-def test_get_release_for_deployment(patch, magic, config):
+def test_get_release_for_deployment(patch, config, database):
     app_id = 'my_app_id'
-    conn = magic()
-    patch.object(Database, 'new_pg_conn', return_value=conn)
     expected_query = """
             with latest as (select app_uuid, max(id) as id
                             from releases
@@ -89,7 +83,7 @@ def test_get_release_for_deployment(patch, magic, config):
             where app_uuid = %s;
             """
 
-    patch.object(conn.cursor(), 'fetchone', return_value={
+    patch.object(database.cur, 'fetchone', return_value={
         'app_uuid': 'my_app_uuid',
         'version': 'my_version',
         'environment': 'my_environment',
@@ -115,7 +109,4 @@ def test_get_release_for_deployment(patch, magic, config):
         owner_uuid='my_owner_uuid',
     )
 
-    conn.cursor().execute.assert_called_with(expected_query, (app_id,))
-
-    conn.cursor().close.assert_called()
-    conn.close.assert_called()
+    database.cur.execute.assert_called_with(expected_query, (app_id,))
--- tests/unit/db/SimpleConnCursor.py
+++ tests/unit/db/SimpleConnCursor.py
@@ -8,7 +8,7 @@ def test_simple(magic):
     conn = magic()
     with SimpleConnCursor(conn) as db:
         assert db.conn == conn
-        assert db.cur == conn.cursor(cursor_factory=RealDictCursor)
+        conn.cursor.assert_called_with(cursor_factory=RealDictCursor)
 
     conn.cursor().close.assert_called_once()
     conn.close.assert_called_once()
