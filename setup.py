ca1ec51e8628a89ec51c62c93075bc75975d7926
Sebastian Wilzbach <seb@wilzba.ch>
2019-07-10 16:13:37 +0200

    Upgrade to Storyscript 0.21.1

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'click==7.0',
         'frustum==0.0.6',
         'raven==6.9.0',
-        'storyscript==0.21.0',
+        'storyscript==0.21.1',
         'ujson==1.35',
         'certifi>=2018.8.24',
         'psycopg2==2.7.5',
