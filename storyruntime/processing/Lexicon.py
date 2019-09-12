67096cac74358271638f6e0ca8c69e09d545edc5
Aditya Bansal <adi.bansal241996@gmail.com>
2019-09-12 17:13:00 +0530

    chore(Lexicon): adjust to updated values of next attrib for if, else if, else

--- storyruntime/processing/Lexicon.py
+++ storyruntime/processing/Lexicon.py
@@ -303,7 +303,11 @@ class Lexicon:
                 result = Lexicon._is_if_condition_true(story, line)
 
             if result:
-                return line['enter']
+                exec_res = await Lexicon.execute_block(logger, story, line)
+                if LineSentinels.is_sentinel(exec_res):
+                    return exec_res
+                return Lexicon.line_number_or_none(
+                    story.line(line.get('next')))
             else:
                 # Check for an elif block or an else block
                 # (step 2 of execution strategy).
