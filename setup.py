ca84d1109763ce4236bbe46ca711475d7a04e5bf
Anukul Sangwan <anukulsangwan@icloud.com>
2019-10-24 06:26:06 +0530

    Configure tox for new packaging

--- setup.py
+++ setup.py
@@ -22,13 +22,6 @@ setup(
     author_email='noreply@storyscript.io',
     version='0.2.0',
     packages=find_packages(),
-    tests_require=[
-        'pytest==3.6.3',
-        'pytest-cov==2.5.1',
-        'pytest-mock==1.10.0',
-        'pytest-asyncio==0.8.0'
-    ],
-    setup_requires=['pytest-runner'],
     python_requires='>=3.7',
     install_requires=[
         'prometheus-client==0.2.0',
@@ -44,6 +37,20 @@ setup(
         'expiringdict==1.1.4',
         'requests==2.21.0'  # Used for structures like CaseInsensitiveDict.
     ],
+    extras_require={
+        'pep8': [
+            'flake8==3.5.0',
+            'flake8-quotes==1.0.0',
+            'flake8-import-order==0.18',
+            'pep8-naming==0.7.0'
+        ],
+        'py37': [
+            'pytest==3.6.3',
+            'pytest-cov==2.5.1',
+            'pytest-mock==1.10.0',
+            'pytest-asyncio==0.8.0',
+        ]
+    },
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
